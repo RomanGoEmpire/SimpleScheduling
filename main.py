43f0f5488f99119800b2cb8f1c15b4e3c20ffbf8
aSusELementWizard <gerloffroman@gmail.com>
2023-01-02 15:30:44 +0100

    changed names of files. task.py to task_description.py task_description.py to task_parser.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from Parser.task_description import TaskDescription
+from Parser.task_parser import TaskParser
 
 if __name__ == "__main__":
-    t = TaskDescription("Examples/scheduling_problem.txt")
+    t = TaskParser("Examples/scheduling_problem.txt")
     list = t.get_task_list()
     for task in list:
         print(task)
